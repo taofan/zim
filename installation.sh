c348aab433e9dcc4977c895e5393fc39d7e76a6b
Tao Fan <tao@fantao.org>
2016-07-24 23:54:50 +1000

    Add removing zwc files

--- installation.sh
+++ installation.sh
@@ -7,11 +7,13 @@ $!/bin/sh
 ZIM_DIR="$HOME/git/tao/zim"
 
 ln -s $ZIM_DIR $HOME/.zim
+rm $HOME/.zshrc
+rm $HOME/.*.zwc
 
 #stow -d $ZIM_DIR -t $HOME -s templates
 # manual link
 setopt EXTENDED_GLOB
-for rcfile in $ZIM_DIR/templates/; do
+for rcfile in "${ZIM_DIR}"/templates/; do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
